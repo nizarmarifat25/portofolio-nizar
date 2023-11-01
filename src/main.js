ecea2af02a7056909be2a3ae03f0c783ead0e293
nizarmarifat25 <nizarmarifat253@gmail.com>
2023-11-01 09:26:53 +0700

    update data aos

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-import { createApp,onMounted } from 'vue'
+import { createApp } from 'vue'
 import "bootstrap/dist/css/bootstrap.css"
 import App from './App.vue'
-import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
 
+import AOS from 'aos';
     AOS.init();
 createApp(App).mount('#app')
 ;
